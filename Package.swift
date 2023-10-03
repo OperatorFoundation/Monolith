7f154398e7c163bb151ce6cf40cfbe02d6fef7b3
bragelbytes <joebragel@gmail.com>
2023-10-02 18:44:10 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto", from: "2.5.0"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/Song", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/Song", branch: "release"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
